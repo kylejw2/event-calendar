30c4f564ef196bd37551ef2d830be8169764477d
Kyle Wright <kylejamwright@gmail.com>
2020-08-25 09:28:06 -0600

    Added canvas to user interface

--- presentation/src/App.js
+++ presentation/src/App.js
@@ -1,25 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react';
 
 function App() {
+
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    // Multiply canvas height and width by 2 to support HD retina screen density on powerful computers
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const c = canvas.getContext('2d');
+    // necessary to scale because of HD retina screens
+    c.scale(2,2);
+    
+
+    class Circle {
+      constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+      }
+      draw() {
+        c.beginPath();
+        c.arc(this.x += this.velocity, this.y += this.velocity, this.radius, 0, Math.PI*2, false);
+        c.fillStyle = this.color;
+        c.fill();
+        c.closePath();
+        if (this.x > canvas.width) {this.x = -5}
+        if (this.y > canvas.height) {this.y = -5}
+      }
+      update() {
+        this.draw();
+      }
+    }
+    let colors = ['#333333', '#643173', '#7D5BA6', '#86A59C', '#89CE94']
+    let objects = [];
+    const init = () => {
+      for (let i = 0; i < 1000; i++) {
+        const randX = Math.random() * canvas.width;
+        const randY = Math.random() * canvas.height;
+        const randRadius = (Math.random() * 4) + 4;
+        const randCol = colors[i % colors.length];
+        const randVelocity = (Math.random() + 0.25) * 0.5;
+        objects.push(new Circle(randX, randY, randRadius, randCol, randVelocity))
+      }
+    }
+    const animate = () => {
+      requestAnimationFrame(animate); //there could be an error here
+      c.clearRect(0,0, canvas.width, canvas.height);c.fillStyle = 'black';
+      c.fillRect(0,0, canvas.width, canvas.height);
+      objects.forEach(circle => circle.draw())
+    }
+
+    init();
+    animate();
+
+    contextRef.current = c;
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas
+      ref={canvasRef}
+    />
   );
 }
 
---
+++ presentation/src/components/verify.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import {BrowserRouter }
+
+const Verify = () => {
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Verify;
