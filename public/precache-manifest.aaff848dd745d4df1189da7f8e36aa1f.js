0aa8e0305130fd5f0205ba1d33d0fa69643894cf
Kyle Wright <kylejamwright@gmail.com>
2020-08-28 15:00:36 -0600

    Official Version 0.1.0

---
+++ public/precache-manifest.aaff848dd745d4df1189da7f8e36aa1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7535a053a667c2e1f7d71dfd9ac7b305",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a822a9d87808ec7af0ab",
+    "url": "/static/css/main.f29cbfb0.chunk.css"
+  },
+  {
+    "revision": "e738b763b226cd1c1d30",
+    "url": "/static/js/2.a21959a2.chunk.js"
+  },
+  {
+    "revision": "567ff855e89811c94bb38895efc27afb",
+    "url": "/static/js/2.a21959a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a822a9d87808ec7af0ab",
+    "url": "/static/js/main.8a5fc34f.chunk.js"
+  },
+  {
+    "revision": "1aadcde5ef86e7287682",
+    "url": "/static/js/runtime-main.44d2f03b.js"
+  }
+]);
